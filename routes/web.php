de09426dec556ff2713f5a1854a45a75337391b7
wiem-2001 <wiembm2001@gmail.com>
2025-09-22 14:12:12 +0100

    fix indentation

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\UserManagementController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
